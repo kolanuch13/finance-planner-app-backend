6f8eab087b26c91b5be8e781e3a110f328033f56
Kosynskyi <kosynskyi.vova@gmail.com>
2023-03-22 17:24:29 +0200

    add mongoose model, add addExpense joi-model, delete files statistics and expenses and create one file transactions instead that files in folders schemas and models, add handleSaveErrors in helpers folder

--- helpers/index.js
+++ helpers/index.js
@@ -2,11 +2,12 @@ const createError = require("./createError");
 const controllerWrapper = require("./controllerWrapper");
 const sendMail = require("./sendMail");
 const requestError = require("./requestError");
-
+const handleSaveErrors = require("./handleSaveErrors");
 
 module.exports = {
   createError,
   controllerWrapper,
   sendMail,
-  requestError
+  requestError,
+  handleSaveErrors,
 };
