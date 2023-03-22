0c0baf310f23b2c991a0fd8b9b6d2c496d32ca5a
Andrey Belokon <belokon1981andrey@gmail.com>
2023-03-22 21:06:32 +0200

    2

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const app = require('./app');
 
-const { DB_HOST, PORT = 4000 } = process.env;
+const { DB_HOST, PORT } = process.env;
 
 mongoose.set('strictQuery', false);
 
