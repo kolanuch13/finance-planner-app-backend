b645b66765663d6365d4d4c58964bd8c8156e246
Mykola <112721616+kolanuch13@users.noreply.github.com>
2023-03-23 13:05:39 +0200

    Create app.js

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const logger = require('morgan');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const authRouter = require('./routes/api/auth');
+const transactionRouter = require('./routes/api/expenses');
+dotenv.config();
+
+const { swaggerUi, swaggerDocument } = require('./services/swagger/swagger');
+
+const app = express();
+
+const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short';
+
+app.use(logger(formatsLogger));
+app.use(cors());
+app.use(express.json());
+require('./config/config-passport'); // ????
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+app.use('/api/auth', authRouter);
+app.use('/api/user', transactionRouter);
+
+app.use((req, res) => {
+  res.status(404).json({ message: 'Not found' });
+});
+
+app.use((err, req, res, next) => {
+  res.status(500).json({ message: err.message });
+});
+
+module.exports = app;
